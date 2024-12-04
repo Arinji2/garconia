3a16c657989b1e85c3e43e35a63c106b84596bf5
Arinji2 <arinjaydhar205@gmail.com>
2024-12-04 09:56:57 +0530

    Link UI With Backend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,10 +2,23 @@ import Header from "./components/header";
 import CountDownComponent from "./components/countdown";
 import Email from "./components/email";
 import Socials from "./components/socials";
+import { Toaster } from "react-hot-toast";
 const END_DATE = new Date("2024-12-31");
 function App() {
   return (
     <>
+      <div>
+        <Toaster
+          position="top-right"
+          toastOptions={{
+            style: {
+              borderRadius: "10px",
+              background: "#333",
+              color: "#fff",
+            },
+          }}
+        />
+      </div>
       <div className="w-full flex flex-col items-center justify-start md:justify-center min-h-screen bg-garconia-background md:py-10 py-5 xl:py-20 xl:h-1  ">
         <div className="w-full h-fit flex flex-col items-center gap-14 justify-start max-w-[1280px]">
           <Header />
--- frontend/src/components/email.tsx
+++ frontend/src/components/email.tsx
@@ -1,17 +1,65 @@
+import { toast } from "react-hot-toast";
+
+const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
 export default function Email() {
   return (
-    <div className="shrink-0 w-[80%] h-fit xl:h-[50px] gap-2 xl:gap-0 flex flex-col xl:flex-row items-center justify-center">
+    <form
+      onSubmit={async (e) => {
+        e.preventDefault();
+
+        const email = (document.getElementById("email") as HTMLInputElement)
+          .value;
+        if (email.length === 0) {
+          toast.error("Please Enter an Email!");
+          return;
+        }
+        if (!emailRegex.test(email)) {
+          toast.error("Invalid Email!");
+          return;
+        }
+        const id = toast.loading("Sending Email...");
+        const backendURL = import.meta.env.VITE_BACKEND_URL;
+        const res = await fetch(`${backendURL}/api/add`, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            email: email,
+          }),
+        });
+        toast.dismiss(id);
+        if (res.status === 200) {
+          toast.success("Email Sent, Check your Inbox!");
+          return;
+        }
+        if (res.status === 400) {
+          toast.error("Email Already Exists!");
+          return;
+        }
+        if (res.status === 409) {
+          toast.error("Email Already Exists!");
+          return;
+        }
+        toast.error("Something Went Wrong!");
+      }}
+      className="shrink-0 w-[80%] h-fit xl:h-[50px] gap-2 xl:gap-0 flex flex-col xl:flex-row items-center justify-center"
+    >
       <input
+        id="email"
         type="email"
         className=" xl:h-full text-garconia-white h-[50px] bg-garconia-offwhite/50 xl:shrink-0 w-full xl:rounded-none xl:w-[80%] outline-none rounded-md xl:rounded-l-md shadow-shadow border-[2px] border-black px-4"
         placeholder="Email..."
       />
-      <button className="px-2 bg-garconia-red w-fit xl:w-full h-[40px] xl:h-full rounded-md xl:rounded-none xl:rounded-r-md shadow-shadow border-[2px] border-black">
+      <button
+        type="submit"
+        className="px-2 bg-garconia-red w-fit xl:w-full h-[40px] xl:h-full rounded-md xl:rounded-none xl:rounded-r-md shadow-shadow border-[2px] border-black"
+      >
         <p className="text-white whitespace-nowrap  md:text-2xl font-bold">
           {" "}
           NOTIFY ME!!{" "}
         </p>
       </button>
-    </div>
+    </form>
   );
 }
